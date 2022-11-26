9355842e20a19cfe621dcfd5a5edafc8c668973d
iamgiovann4 <giovannasiqueiranicolau@gmail.com>
2022-11-26 17:46:28 -0300

    btn funcionando

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function valuebotom(){
-    var btn1 = document.getElementById(btn);
-    btn1.value = 'oiii';
+    var btn1 = document.getElementById('btn')
+    btn1.innerHTML = 'oiii'
+    btn1.value = btn1.innerHTML
+    console.log(btn1.value)
 }
