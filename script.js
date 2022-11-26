676db85ae2ac45e78c4a0f2b6574d5d094fd2bb6
Vitor Moreira <110418519+M4riotto@users.noreply.github.com>
2022-11-26 17:42:30 -0300

    bottom js

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-function valuebotom(){
-    var btn1 = document.getElementById(btn);
-    btn1.value = 'oiii';
+// function valuebotom(){
+//     var btn1 = document.getElementById(botao);
+//     btn1.value = innerHtml = 'oii'
+// }
+function mudarNome()
+{
+ if(document.getElementById("button").value == "Ativo")
+ {
+  document.getElementById("button").value = "Inativo";
+ } 
+ else
+ {
+  document.getElementById("button").value = "Ativo";
+ }
 }
